7592de4f97642aa5bc75f25656504a4e645f2130
DooDI-SujiKim <136331819+DooDI-SujiKim@users.noreply.github.com>
2023-12-08 15:05:07 +0900

    Add a new exercise related to class and object in practice.kt

--- app/src/main/java/udemy/android12/kotlinbasics/practice.kt
+++ app/src/main/java/udemy/android12/kotlinbasics/practice.kt
@@ -1,10 +1,22 @@
 package udemy.android12.kotlinbasics
 
 fun main(){
+    /*
     var nullableName : String? = "Denis"
     nullableName = null
 
     var len2 = nullableName?.length
     nullableName.let { it:String? -> println(it?.length) }
     // println(nullableName?.length)
+     */
+
+    var phone1 = MobilePhone("Android", "Samsung", "Galaxy S20 Ultra" )
+    val phone2 = MobilePhone("iOS", "Apple", "iPhone 13")
+    val phone3 = MobilePhone("Android", "Google", "Pixel 6")
+}
+// #54. class와 object practice
+class MobilePhone(osName : String, brand : String, model : String){
+    init{
+        println ("$osName $brand $model")
+    }
 }
